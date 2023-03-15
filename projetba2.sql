e7667eb60ae07768f91962dd8c791c8421b56108
cyril Taquet <cyril.taquet@hotmail.be>
2023-03-15 14:03:38 +0100

    version final algo + version finale bdd

--- projetba2.sql
+++ projetba2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 24 fév. 2023 à 13:55
+-- Généré le : mer. 15 mars 2023 à 13:02
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -39,14 +39,32 @@ CREATE TABLE IF NOT EXISTS `classe` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `classprof`
+-- Structure de la table `classenseignant`
 --
 
-DROP TABLE IF EXISTS `classprof`;
-CREATE TABLE IF NOT EXISTS `classprof` (
+DROP TABLE IF EXISTS `classenseignant`;
+CREATE TABLE IF NOT EXISTS `classenseignant` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `IdProf` int(11) NOT NULL,
   `IdClasse` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(40) COLLATE utf16_bin NOT NULL,
+  `Prenom` varchar(32) COLLATE utf16_bin NOT NULL,
+  `Email` varchar(128) COLLATE utf16_bin NOT NULL,
+  `Telelphone` varchar(16) COLLATE utf16_bin NOT NULL,
+  `Description` varchar(1024) COLLATE utf16_bin NOT NULL,
+  `Specialite` varchar(32) COLLATE utf16_bin NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
@@ -69,17 +87,31 @@ CREATE TABLE IF NOT EXISTS `eleve` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `professeur`
+-- Structure de la table `enseignant`
 --
 
-DROP TABLE IF EXISTS `professeur`;
-CREATE TABLE IF NOT EXISTS `professeur` (
+DROP TABLE IF EXISTS `enseignant`;
+CREATE TABLE IF NOT EXISTS `enseignant` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Nom` varchar(50) COLLATE utf16_bin NOT NULL,
   `Prenom` varchar(50) COLLATE utf16_bin NOT NULL,
   `Email` varchar(255) COLLATE utf16_bin NOT NULL,
   `Mdp` varchar(255) COLLATE utf16_bin NOT NULL,
   `Admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fiche`
+--
+
+DROP TABLE IF EXISTS `fiche`;
+CREATE TABLE IF NOT EXISTS `fiche` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(32) COLLATE utf16_bin NOT NULL,
+  `Sujet` varchar(32) COLLATE utf16_bin NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
@@ -94,6 +126,7 @@ CREATE TABLE IF NOT EXISTS `test` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `IdProf` int(11) NOT NULL,
   `IdEleve` int(11) NOT NULL,
+  `Date` text COLLATE utf16_bin NOT NULL,
   `ScoreTDA` float NOT NULL,
   `ScoreDyslexie` float NOT NULL,
   `ScoreDysortho` float NOT NULL,
