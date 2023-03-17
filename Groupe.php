62ad6cd22028ca6070d4c462f8fec0a6608a941c
220795@umons.ac.be <manonbrandes@hotmail.fr>
2023-03-17 09:15:56 +0100

    créa classe groupe

---
+++ Groupe.php
@@ -0,0 +1,37 @@
+<?php
+
+class Groupe {
+
+    private $Idclasse;
+    private $Nom;
+    private $bdd;
+
+    //Requête création groupe
+
+    public function newGroup() {
+        
+        //Crea requête
+        $query = "INSERT INTO `classe`(`Id`, `Nom`) VALUES (NULL,':Nom')";
+        
+        //Prepare
+        $requete = $this->bdd->prepare($query);
+
+        //link param
+        $requete->bindParam(':Nom', $this->Nom);
+        
+        //exécuter
+
+        if($requete->execute()) {
+
+            return true;
+        }
+        
+        else {return false;}
+
+
+    }
+}
+
+
+
+?>
