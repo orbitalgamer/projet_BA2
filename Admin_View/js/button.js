0b09a305dae3d14c80281f859b51c6d82faf1aaa
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2023-03-18 15:27:39 +0100

    Add files via upload

---
+++ Admin_View/js/button.js
@@ -0,0 +1,28 @@
+
+
+//Button-----------------------------------------------------------------------------------------------------------------------------------------
+export const searchBtn = document.querySelector(".searchBtn");
+export const menuIcon = document.querySelector(".menuBtn");
+/*-----------------------------------768px--------------------------------------------*/
+export const searchBtn768px = document.querySelector(".searchBtn768px");
+export const menuIcon768px = document.querySelector(".menuBtn768px");
+//Objet------------------------------------------------------------------------------------------------------------------------------------------
+export const searchBar = document.querySelector(".searchBar");
+export const lineMenuIcon = document.querySelector(".menuIcon");
+export const line2Icon = document.querySelector("#two");
+export const sideMenu = document.querySelector(".sideMenu");
+export const headContainer = document.querySelector(".headerContainer");
+export const mainContainer = document.querySelector(".mainContainer");
+export const tableContainer = document.querySelector(".tableContainer");
+export const blockContainer =  document.querySelector(".blockContainer");
+export const mainSideMenu = document.querySelector(".mainSideMenu");
+export const titre = document.querySelector(".titre");
+/*------------------------------768px--------------------------------------------------*/
+export const mainSideMenu768px = document.querySelector(".mainSideMenu768px");
+export const sideMenu768px = document.querySelector(".sideMenu768px");
+export const headContainer768px = document.querySelector(".headerContainer768px");
+export const mainContainer768px = document.querySelector(".mainContainer768px");
+export const block768px = document.querySelector(".block768px");
+export const searchBar768px = document.querySelector(".searchBar768px");
+export const infoPageText = document.querySelector(".infoPageText");
+export const twoLine768px = document.querySelector("#two768px");
