09f22ca198cd4ae68d2a07317bf8539fba3ac75d
GravityFalls25 <thibautwindal@hotmail.com>
2023-04-12 18:14:48 +0200

    apprentissage jvscript

--- js/connexion.js
+++ js/connexion.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const ID = {"Identifiant": "nom.prenom", "Mdp":"test"}
 
   const switcher = document.querySelector(".boxconnexion");
   switcher.addEventListener('mouseover',function(){
@@ -13,3 +13,23 @@ switcher.addEventListener('mouseout',function(){
     document.getElementById('over').id  = 'notover';
     
   } );
+  switcher.addEventListener('click',()=>{
+    const id = document.querySelector('.boxID').value
+    const mdp = document.querySelector('.boxpassword').value
+    ID.Identifiant = test
+    ID.Mdp = test2
+    console.log(ID)
+  })
+  fetch("https://api.themistest.be/api/Connection/Connection.php",{
+    method: "POST",
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify(ID)
+    //JSON.parse
+  }
+  
+  ).then(response=>{return response.json()}).then(data=>{fetch("https://api.themistest.be/api/Enseignant/Affichage.php?Id=All",{
+    method:"POST",
+    body:JSON.stringify({Token:data.data.Token})
+  }).then(response=>response.json()).then(data=>console.log(data))})
+ 
+  
