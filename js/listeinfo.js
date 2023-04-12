724330fee90502159ffcc7b68150c5d778f30f56
GravityFalls25 <thibautwindal@hotmail.com>
2023-04-12 22:11:15 +0200

    liste info bouton visuel

---
+++ js/listeinfo.js
@@ -0,0 +1,16 @@
+    'use strict';
+
+    const switchers = document.querySelectorAll(".trouble");
+    switchers.forEach(function(switcher){
+    switcher.addEventListener('mouseover',function(){
+        
+    switcher.className = "trouble over";
+    } );
+
+    switcher.addEventListener('mouseout',function(){
+
+        switcher.className = "trouble notover";
+    } )})
+
+
+   
