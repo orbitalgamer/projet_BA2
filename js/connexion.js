c404d635142428d3c6e99ac01170d3e977308696
GravityFalls25 <thibautwindal@hotmail.com>
2023-05-02 21:53:45 +0200

    amelioration bouton connexion

--- js/connexion.js
+++ js/connexion.js
@@ -4,32 +4,68 @@ const ID = {"Identifiant": "nom.prenom", "Mdp":"test"}
   const switcher = document.querySelector(".boxconnexion");
   switcher.addEventListener('mouseover',function(){
       
-    document.getElementById('notover').id  = 'over';
+    switcher.classList.add('over');
     
   } );
 
 switcher.addEventListener('mouseout',function(){
 
-    document.getElementById('over').id  = 'notover';
+  switcher.classList.remove('over');
     
   } );
-  switcher.addEventListener('click',()=>{
-    const id = document.querySelector('.boxID').value
-    const mdp = document.querySelector('.boxpassword').value
-    ID.Identifiant = test
-    ID.Mdp = test2
-    console.log(ID)
-  })
-  fetch("https://api.themistest.be/api/Connection/Connection.php",{
-    method: "POST",
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify(ID)
-    //JSON.parse
-  }
-  
-  ).then(response=>{return response.json()}).then(data=>{fetch("https://api.themistest.be/api/Enseignant/Affichage.php?Id=All",{
-    method:"POST",
-    body:JSON.stringify({Token:data.data.Token})
-  }).then(response=>response.json()).then(data=>console.log(data))})
- 
-  
+  switcher.addEventListener('mousedown',function(){
+
+    switcher.classList.add('clicked');
+      
+    } );
+    switcher.addEventListener('mouseup',function(){
+
+      switcher.classList.remove('clicked');
+        
+      } );
+
+      switcher.addEventListener('touchstart', function() {
+        switcher.classList.add('clicked');
+      });
+      
+      switcher.addEventListener('touchend', function() {
+        switcher.classList.remove('clicked');
+      });
+      switcher.addEventListener('click',()=>{
+
+        const id = document.querySelector('.boxID')
+        const mdp = document.querySelector('.boxpassword')
+        console.log(id.value)
+        const idSplite = id.value.split(".");
+        fetch("https://api.themistest.be/api/Connection/Connection.php",{
+          method: "POST",
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({"Identifiant": id.value , "Mdp": mdp.value})
+      
+      }).then(response=>{return response.json()}).then(data=> {
+          localStorage.setItem("Token", data.data.Token);
+          if(data.message == "succes"){
+            fetch("https://api.themistest.be/api/Enseignant/Affichage.php",{
+              method:"POST",
+              body:JSON.stringify({Token: localStorage.getItem("Token")})
+            }).then(response => response.json()).then(data => {
+              if(data.data[0].Admin == "1"){
+                switcher.setAttribute("href", "../Admin_View/Admin_Screen.html")
+              }
+              else if( data.data[0].Admin == "0"){
+                switcher.setAttribute("href","../Prof_View/prof_view.html");
+              }
+              else{
+                return;
+              }
+            })
+          }
+          else{
+            return;
+          }
+      })
+        
+      })
+    
+     
+      
